d1be74c0f5c9aa0e1902fa3658b9c146376c63e8
mrklyndndcst <decostomarklyndonc@gmail.com>
2021-03-03 11:39:20 +0800

    done Day 15 project

---
+++ D015_Coffee_Machine/CoffeeMachine.py
@@ -0,0 +1,45 @@
+from data import resources
+from data import MENU
+from data import money
+
+
+def check_ingredients(order_ingredients):
+    """Returns True when order can be made, False if ingredients are insufficient."""
+    for item in order_ingredients:
+        if order_ingredients[item] > resources[item]:
+            print(f"Sorry there is not enough {item}.")
+            return False
+        else:
+            resources[item] -= order_ingredients[item]
+            return True
+
+def check_payment():
+    """Returns transaction complete if payments are met"""
+    print("Please insert coins. ")
+    total = int(input("how many quarters?: ")) * 0.25
+    total += int(input("how many dimes?: ")) * 0.1
+    total += int(input("how many nickles?: ")) * 0.05
+    total += int(input("how many pennies?: ")) * 0.01
+    if total >= drink['cost']:
+        total -= drink['cost']
+        print(f"Here is ${round(total, 2)} in change.")
+        print(f"Here is your {choice} ☕️. Enjoy!")
+        return True
+
+machine = True
+while machine:
+
+    choice = input("What would you like? (espresso ($1.5)/latte ($2.5)/cappuccino ($3)): ").lower()
+
+    if choice == "off":
+        machine = False
+    elif choice == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${money}")
+    else:
+        drink = MENU[choice]
+        if check_ingredients(drink['ingredients']):
+            if check_payment():
+                money += drink['cost']
